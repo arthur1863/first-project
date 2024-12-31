ea0ff332b0644f466590e55e501817308fc125cc
arthur1863 <steamhamza6@gmail.com>
2024-12-31 20:01:34 +0100

    new contentn

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("hello world!");
+class User {
+  constructor(name, age, job) {
+    (this.name = name), (this.age = age), (this.job = job);
+  }
+
+  sayHi() {
+    alert(`${this.name} says hi and he is ${this.age} years old`);
+  }
+
+  sayJob() {
+    alert(`${this.name} workd as ${this.job}`);
+  }
+}
+
+const user1 = new User("hamza baitiche", 30, "web developer");
+
+console.log(Object.getOwnPropertyNames(User.prototype));
